77bd9c36f232ecb11da4539ca3f69dfb8062ad0b
ben_pollard <ben@singerinstruments.com>
2023-11-16 11:11:01 +0000

    Added log tests

---
+++ ktvn/src/main/kotlin/com/github/benpollarduk/ktvn/text/log/Log.kt
@@ -0,0 +1,55 @@
+package com.github.benpollarduk.ktvn.text.log
+
+import java.util.LinkedList
+import java.util.Queue
+
+/**
+ * Provides an event log. The [maximumCapacity] specifies the maximum number of elements this [Log] can contain, after
+ * which values are discarded as new ones are added, in a first in first out manner.
+ */
+public class Log(public val maximumCapacity: Int = DEFAULT_CAPACITY) {
+    private val queue: Queue<LogElement> = LinkedList()
+
+    /**
+     * Get the number of logged elements.
+     */
+    public val count: Int
+        get() = queue.size
+
+    /**
+     * Clear all elements from the log.
+     */
+    public fun clear() {
+        queue.clear()
+    }
+
+    /**
+     * Add an element to the log.
+     */
+    public fun add(logElement: LogElement) {
+        if (maximumCapacity != NO_LIMIT && queue.size == maximumCapacity) {
+           queue.remove()
+        }
+
+        queue.add(logElement)
+    }
+
+    /**
+     * Get the contents of the log, as and array.
+     */
+    public fun toArray() : Array<LogElement> {
+        return queue.toTypedArray()
+    }
+
+    public companion object {
+        /**
+         * Gets the constant value for no limit.
+         */
+        public const val NO_LIMIT: Int = -1
+
+        /**
+         * Gets the default maximum capacity.
+         */
+        public const val DEFAULT_CAPACITY: Int = 250
+    }
+}
---
+++ ktvn/src/test/kotlin/com/github/benpollarduk/ktvn/text/LogTest.kt
@@ -0,0 +1,63 @@
+package com.github.benpollarduk.ktvn.text
+
+import com.github.benpollarduk.ktvn.text.log.Log
+import com.github.benpollarduk.ktvn.text.log.LogElement
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class LogTest {
+    @Test
+    fun `given log with no elements when getting count then return 0`() {
+        // Given
+        val log = Log()
+
+        // When
+        val result = log.count
+
+        // Then
+        Assertions.assertEquals(0, result)
+    }
+
+    @Test
+    fun `given log with 1 elements when getting count then return 1`() {
+        // Given
+        val log = Log()
+        log.add(LogElement.StringLog(""))
+
+        // When
+        val result = log.count
+
+        // Then
+        Assertions.assertEquals(1, result)
+    }
+
+    @Test
+    fun `given log with maximum capacity 5 when adding 10 elements then getting count then return 5`() {
+        // Given
+        val log = Log(5)
+        for (i in 1..10) {
+            log.add(LogElement.StringLog(""))
+        }
+
+        // When
+        val result = log.count
+
+        // Then
+        Assertions.assertEquals(5, result)
+    }
+
+    @Test
+    fun `given log with 5 when getting as array then return array with 5 elements`() {
+        // Given
+        val log = Log()
+        for (i in 1..5) {
+            log.add(LogElement.StringLog(""))
+        }
+
+        // When
+        val result = log.toArray()
+
+        // Then
+        Assertions.assertEquals(5, result.size)
+    }
+}
