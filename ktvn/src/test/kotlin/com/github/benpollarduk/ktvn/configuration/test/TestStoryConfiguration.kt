5e07648eb38d35a0102215e92846c98157205b5a
ben_pollard <ben@singerinstruments.com>
2023-11-19 17:13:06 +0000

    Fixed bug

--- ktvn/src/test/kotlin/com/github/benpollarduk/ktvn/configuration/test/TestStoryConfiguration.kt
+++ ktvn/src/test/kotlin/com/github/benpollarduk/ktvn/configuration/test/TestStoryConfiguration.kt
@@ -7,8 +7,20 @@ import com.github.benpollarduk.ktvn.logic.structure.ChapterTransition
 import com.github.benpollarduk.ktvn.logic.structure.Scene
 import com.github.benpollarduk.ktvn.logic.structure.SceneListener
 import com.github.benpollarduk.ktvn.logic.structure.SceneTransition
+import com.github.benpollarduk.ktvn.logic.structure.Step
+import com.github.benpollarduk.ktvn.logic.structure.StepListener
 
 internal class TestStoryConfiguration : StoryConfiguration {
+    override val chapterListener: ChapterListener = object : ChapterListener {
+        override fun enter(chapter: Chapter, transition: ChapterTransition) {
+            // nothing
+        }
+
+        override fun exit(chapter: Chapter) {
+            // nothing
+        }
+    }
+
     override val sceneListener: SceneListener = object : SceneListener {
         override fun enter(scene: Scene, transition: SceneTransition) {
             // nothing
@@ -23,12 +35,12 @@ internal class TestStoryConfiguration : StoryConfiguration {
         }
     }
 
-    override val chapterListener: ChapterListener = object : ChapterListener {
-        override fun enter(chapter: Chapter, transition: ChapterTransition) {
+    override val stepListener: StepListener = object : StepListener {
+        override fun enter(step: Step) {
             // nothing
         }
 
-        override fun exit(chapter: Chapter) {
+        override fun exit(step: Step) {
             // nothing
         }
     }
