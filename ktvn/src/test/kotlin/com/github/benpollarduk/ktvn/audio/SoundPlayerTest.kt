5894e5317eef50016ad50eac4832c7043eb5084f
ben_pollard <ben@singerinstruments.com>
2024-01-01 17:06:17 +0000

    Removed test resource and use generated sine wave for audio tests

--- ktvn/src/test/kotlin/com/github/benpollarduk/ktvn/audio/SoundPlayerTest.kt
+++ ktvn/src/test/kotlin/com/github/benpollarduk/ktvn/audio/SoundPlayerTest.kt
@@ -2,8 +2,10 @@ package com.github.benpollarduk.ktvn.audio
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import java.io.File
-import java.io.FileOutputStream
+import java.io.ByteArrayInputStream
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioInputStream
+import kotlin.math.sin
 
 class SoundPlayerTest {
     @Test
@@ -55,23 +57,44 @@ class SoundPlayerTest {
     }
 
     @Test
-    fun `given play from file when valid file then true`() {
+    fun `given play when using generated sine wave then return true`() {
         // Given
         val player = SoundPlayer()
-        val resource = "test-audio.wav"
-        val testStream = javaClass.classLoader.getResourceAsStream(resource)
-        val tempFile = File.createTempFile("test-audio", ".wav")
-        tempFile.deleteOnExit()
 
-        // write the contents of the resource to the temporary file
-        testStream.use { input ->
-            FileOutputStream(tempFile).use { output ->
-                input.copyTo(output)
-            }
+        // create dummy audio data (sine wave for testing)
+        val sampleRate = 44100.0
+        val durationInSeconds = 1.0
+        val frequency = 440.0
+        val amplitude = 0.5
+        val numSamples = (sampleRate * durationInSeconds).toInt()
+
+        // generate sine wave test data
+        val audioData = ByteArray(numSamples * 2) // 16-bit samples, so 2 bytes per sample
+        val angularFrequency = 2.0 * Math.PI * frequency
+        for (i in 0 until numSamples) {
+            val sample = (amplitude * sin(angularFrequency * i / sampleRate)).toInt().toShort()
+            audioData[i * 2] = sample.toByte()
+            audioData[i * 2 + 1] = (sample.toInt() ushr 8).toByte()
         }
 
+        // create a dummy audio stream with a PCM_SIGNED encoding, 44100 Hz, 16-bit, mono
+        val dummyAudioFormat = AudioFormat(
+            AudioFormat.Encoding.PCM_SIGNED,
+            sampleRate.toFloat(),
+            16,
+            1,
+            2,
+            sampleRate.toFloat(),
+            false
+        )
+        val dummyAudioStream = AudioInputStream(
+            ByteArrayInputStream(audioData),
+            dummyAudioFormat,
+            audioData.size.toLong()
+        )
+
         // When
-        val result = player.playFromFile(tempFile.absolutePath, VolumeManager.FULL_ATTENUATION)
+        val result = player.play(dummyAudioStream, VolumeManager.FULL_ATTENUATION, false)
 
         // Then
         Assertions.assertTrue(result.wasSuccessful)
